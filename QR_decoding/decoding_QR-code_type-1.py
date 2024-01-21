729d057ab8df5669d92b4455c2f0dfdb0b5d120c
Andrew Kovalenko <kov.andreas.30@gmail.com>
2024-01-21 18:29:51 +0200

    create part of QR-content reading function for QR-decoding

--- QR_decoding/decoding_QR-code_type-1.py
+++ QR_decoding/decoding_QR-code_type-1.py
@@ -56,8 +56,41 @@ def show_matrix_in_pretty_format(arr_2d = [[0, 0],[1, 1]]):
 		print(arr_2d[i])
 
 
-content_part_of_QR_code = get_content_part_from_QR_code(example_of_QR_code)
-show_matrix_in_pretty_format(content_part_of_QR_code)
+def reading_data_from_QR_content_part(QR_content_part = [[1, 0], [0, 1]]):
+	result_bits_string = ''
+
+	rows_index_i = len(QR_content_part) - 1
+	columns_index_j = len(QR_content_part[0]) - 1
+	movement_vector_for_read_QR_content = 'up'
+	QR_content_bit_amount = len(QR_content_part) * len(QR_content_part[0])
+	for _ in range(0, QR_content_bit_amount)
+		# first phase - from down to up	
+		if rows_index_i > 0 and columns_index_j % 2 != 0 and movement_vector_for_read_QR_content == 'up':
+			columns_index_j -= 1
+		elif rows_index_i > 0 and columns_index_j % 2 == 0 and movement_vector_for_read_QR_content == 'up':
+			columns_index_j += 1
+			rows_index_i -= 1
+
+		# link between first and second algorithm phases
+		if rows_index_i == 0 and columns_index_j == len(QR_content_part) - 1:
+			upper_str_from_left_to_right = ''.join(QR_content_part[rows_index_i][-4:][::-1])
+			result_bits_string += upper_str_from_left_to_right
+			columns_index_j -= 3
+			rows_index_i += 1
+			movement_vector_for_read_QR_content = 'down'
+		
+		# second phase - from up to down
+		if rows_index_i < len(QR_content_part) - 1 and columns_index_j % 2 != 0 and movement_vector_for_read_QR_content == 'down':
+			columns_index_j -= 1
+		elif columns_index_j % 2 == 0 and movement_vector_for_read_QR_content == 'down':
+			columns_index_j += 1
+			rows_index_i += 1
+		
+		result_bits_string += QR_content_part[rows_index_i][columns_index_j]
+
+
+# content_part_of_QR_code = get_content_part_from_QR_code(example_of_QR_code)
+# show_matrix_in_pretty_format(content_part_of_QR_code)
 
 
 
