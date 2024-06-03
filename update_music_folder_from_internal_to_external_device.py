7f9edaeeaf36100e25b6231bb738c19e58d52987
Andrew Kovalenko <kov.andreas.30@gmail.com>
2024-06-03 22:44:23 +0300

    move file into program folder.

--- update_music_folder_from_internal_to_external_device.py
+++
@@ -1,6 +0,0 @@
-# this will be experimental program for updating music files from local disk to external disk device
-# Idea is to define only new music files, which absent on the external disk and after that copy their to device to
-# certain folder.
-# Also show process stages and their results.
-
-
