d65e75ed1d66317b5a63acb68f8099800935e1d3
Andrew Kovalenko <kov.andreas.30@gmail.com>
2024-07-12 23:37:00 +0300

    done little modification

--- Updating_music_files_on_External_Disk_from_local_disk/update_music_folder_from_internal_to_external_device.py
+++ Updating_music_files_on_External_Disk_from_local_disk/update_music_folder_from_internal_to_external_device.py
@@ -25,7 +25,7 @@ class Music_Dir_on_Local_Disk:
 		self.current_login_user = self.define_log_in_user_to_Linux_system()
 		self.search_dir_level = 0
 		self.excluded_dirnames_for_search = ['SOFT']  # list can be extended
-		self.current_search_dir = ''
+		self.current_search_abs_dir_path = ''
 
 	def get_operation_system_type(self):
 		# print('OS type:', self.operation_system_type)
@@ -60,7 +60,8 @@ class Music_Dir_on_Local_Disk:
 
 		for next_level_dir in all_next_level_dirs:
 			if self.dirs_by_levels_and_checked_status[next_level_dir][1] == 'Unchecked':
-				self.current_search_dir = next_level_dir
+				self.current_search_abs_dir_path = next_level_dir
+				break
 
 	def add_search_abs_dir_path_with_dir_level_and_unchecked_status(certain_abs_dir_path, dir_level):
 		self.dirs_by_levels_and_checked_status[certain_abs_dir_path] = [dir_level, 'Unchecked']
