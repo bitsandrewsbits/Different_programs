41e916269cc83c91e31b83d33b91755103444898
Andrew Kovalenko <kov.andreas.30@gmail.com>
2024-08-11 18:28:40 +0300

    add new file for class External Device Searcher.

---
+++ Updating_music_files_on_External_Disk_from_local_disk/external_device_searcher.py
@@ -0,0 +1,10 @@
+# in this file will be created class for searching and defining
+# connected external devices like flash, external HDD, SSD, or another.
+# if nothing external connected - to tell user about it, and terminating program.
+
+class External_Device_Searcher:
+	def __init__(self):
+		pass
+
+	def external_devices_connected(self):
+		pass
