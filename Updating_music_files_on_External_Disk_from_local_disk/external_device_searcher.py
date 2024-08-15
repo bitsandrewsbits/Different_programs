ed7ddaed6c92823cf47cd8f62560dafcf2745c53
Andrew Kovalenko <kov.andreas.30@gmail.com>
2024-08-15 22:13:56 +0300

    change class name in external_device_searcher file. Also add some description about how to resolve one problem.

--- Updating_music_files_on_External_Disk_from_local_disk/external_device_searcher.py
+++ Updating_music_files_on_External_Disk_from_local_disk/external_device_searcher.py
@@ -1,10 +1,23 @@
-# in this file will be created class for searching and defining
-# connected external devices like flash, external HDD, SSD, or another.
-# if nothing external connected - to tell user about it, and terminating program.
+# in this file will be created class for defining
+# connected external USB devices like flash, external HDD, SSD, or another.
+# if nothing external USB disk connected - to tell user about it, and terminating program.
 
-class External_Device_Searcher:
+class External_Connected_USB_Disk_Devices_Searcher:
 	def __init__(self):
 		pass
 
-	def external_device_connected_to_computer(self):
-		# question: how exactly to determine which mounted partitions to computer - is external USB hard disk, USB-flash partitions?
+	def external_USB_devices_connected_to_computer(self):
+		# experimental variant to resolve this task(first for Linux systems):
+		# 1) use dmesg command with grep: sudo dmesg | grep 'usb' > usb_detected_strs.txt
+		# 2) use dmesg command with grep: sudo dmesg | grep 'usb-storage' > usb_storage_strs.txt
+		# 3) Parse created file from (2) - detect words "1-1.2:" or something like that and save it in python list or dict
+		# 4) Create strings - 
+		#	1)usb-<detected_words_from_(3)> Product: 
+		#	2)usb-<detected_words_from_(3)> Manufacturer:
+		# 5) Parse created file from (1) - detect string from (4). Write them to separate file.
+		# 6) Parse created file from (5) - detect Product and Manufacturer values and save it to list or dict.
+		# 7) If list or dict from (6) - is empty -> method returns False
+		#    If list or dict from (6) - not empty -> method returns True
+
+		# I think it will be in another class. About detection partitions of connected USB devices.
+		# N) Find from output (1) - detect words - sdb, sdc,... - as block devices, mounted to your Linux system.
