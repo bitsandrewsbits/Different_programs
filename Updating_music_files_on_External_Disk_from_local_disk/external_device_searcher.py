1adeb066adae292ee6b7cbc5f9ca02af8071fb04
Andrew Kovalenko <kov.andreas.30@gmail.com>
2024-08-16 18:02:25 +0300

    add 2 methods to External_USB_Disk_Searcher class - execution of dmesg commands for getting required information.

--- Updating_music_files_on_External_Disk_from_local_disk/external_device_searcher.py
+++ Updating_music_files_on_External_Disk_from_local_disk/external_device_searcher.py
@@ -1,6 +1,7 @@
 # in this file will be created class for defining
 # connected external USB devices like flash, external HDD, SSD, or another.
 # if nothing external USB disk connected - to tell user about it, and terminating program.
+import os
 
 class External_Connected_USB_Disk_Devices_Searcher:
 	def __init__(self):
@@ -8,8 +9,16 @@ class External_Connected_USB_Disk_Devices_Searcher:
 
 	def external_USB_devices_connected_to_computer(self):
 		# experimental variant to resolve this task(first for Linux systems):
+		pass
+
 		# 1) use dmesg command with grep: sudo dmesg | grep 'usb' > usb_detected_strs.txt
+	def get_and_write_info_from_dmesg_cmd_about_connected_USB_devs(self):
+		os.system("sudo dmesg | grep 'usb' > usb_detected_strs.txt")
+
 		# 2) use dmesg command with grep: sudo dmesg | grep 'usb-storage' > usb_storage_strs.txt
+	def get_and_write_info_from_dmesg_cmd_about_connected_USB_storage_devs(self):
+		os.system("sudo dmesg | grep 'usb-storage' > usb_storage_strs.txt")
+
 		# 3) Parse created file from (2) - detect words "1-1.2:" or something like that and save it in python list or dict
 		# 4) Create strings - 
 		#	1)usb-<detected_words_from_(3)> Product: 
@@ -21,3 +30,8 @@ class External_Connected_USB_Disk_Devices_Searcher:
 
 		# I think it will be in another class. About detection partitions of connected USB devices.
 		# N) Find from output (1) - detect words - sdb, sdc,... - as block devices, mounted to your Linux system.
+
+if __name__ == "__main__":
+	external_usb_dev_seacher = External_Connected_USB_Disk_Devices_Searcher()
+	external_usb_dev_seacher.get_and_write_info_from_dmesg_cmd_about_connected_USB_devs()
+	external_usb_dev_seacher.get_and_write_info_from_dmesg_cmd_about_connected_USB_storage_devs()
