2d20d9b5896bce93e9004d4de7785903e15e1ee0
Andrew Kovalenko <kov.andreas.30@gmail.com>
2024-08-19 19:01:55 +0300

    refactor few method in External Dev class.

--- Updating_music_files_on_External_Disk_from_local_disk/external_device_searcher.py
+++ Updating_music_files_on_External_Disk_from_local_disk/external_device_searcher.py
@@ -29,37 +29,40 @@ class External_Connected_USB_Disk_Devices_Searcher:
 		return result_file_strs
 
 	# 3) Parse created file from (2) - detect words "1-1.2:" or something like that and save it in python list or dict
-	def get_target_strings_from_file_strs(self, file_strs: list[str], target_str: str):
+	def get_target_strings_from_strs(self, strings: list[str], regex_str: str):
 		found_target_strs = []
-		regex_obj = re.compile('.*' + target_str + '.*')
+		regex_obj = re.compile(regex_str)
 		
-		for string in file_strs:
+		for string in strings:
 			found_target_str = regex_obj.match(string)
 			if found_target_str:
 				found_target_strs.append(string)
 
 		return found_target_strs
 
-	def get_USB_dev_number_from_target_strs(self, target_strs: str):
-		USB_dev_number = ''
-		regex_obj = re.compile('.*usb-storage [1-9]-[1-9][.:][1-9 ].*')
-		str_with_USB_number = ''
-		for string in target_strs:
-			if regex_obj.match(string):
-				str_with_USB_number = string
-				print('Found target USB number in string:', string)
-				break
-
-		str_elements = str_with_USB_number.split(' ')
+	def get_connected_USB_dev_numbers_from_target_strs(self, target_strs: list[str]):
+		connected_USB_dev_numbers = []
 		regex_obj = re.compile('.*[1-9]-[1-9][.:][1-9 ].*')
-		for elem in str_elements:
-			if regex_obj.match(elem):
-				USB_dev_number = elem
 
-		found_USB_dev_number = USB_dev_number.split(':')[0]
-		print('Found connected USB dev with number:', found_USB_dev_number)
+		for str_with_USB_number in target_strs:
+			str_elements = str_with_USB_number.split(' ')
+			for elem in str_elements:
+				if regex_obj.match(elem):
+					USB_dev_number = elem
+					connected_USB_dev_numbers.append(USB_dev_number)
+					break
 
-		return found_USB_dev_number
+		connected_USB_dev_numbers_without_suffix = set(self.get_clean_USB_dev_numbers_without_suffix(connected_USB_dev_numbers))
+		print('Found connected USB devices with numbers:', connected_USB_dev_numbers_without_suffix)
+
+		return connected_USB_dev_numbers_without_suffix
+
+	def get_clean_USB_dev_numbers_without_suffix(self, usb_num_strs: list[str]):
+		clean_USB_dev_numbers = []
+		for usb_num_str in usb_num_strs:
+			clean_USB_dev_numbers.append(usb_num_str.split(':')[0]) # add only x-x.x or x-x numbers
+
+		return clean_USB_dev_numbers
 
 		# 4) Create strings - 
 		#	1)usb-<detected_words_from_(3)> Product: 
@@ -78,6 +81,6 @@ if __name__ == "__main__":
 	external_usb_dev_seacher.get_and_write_info_from_dmesg_cmd_about_connected_USB_storage_devs()
 	usb_strs = external_usb_dev_seacher.get_strings_from_txt_file('usb_detected_strs.txt')
 	usb_storage_strs = external_usb_dev_seacher.get_strings_from_txt_file('usb_storage_strs.txt')
-	target_usb_storage_strs = external_usb_dev_seacher.get_target_strings_from_file_strs(usb_storage_strs, 'usb-storage')
+	target_usb_storage_strs = external_usb_dev_seacher.get_target_strings_from_strs(usb_storage_strs, '.*[1-9]-[1-9][.:][1-9 ].*')
 	print(target_usb_storage_strs)
-	external_usb_dev_seacher.get_USB_dev_number_from_target_strs(target_usb_storage_strs)
+	external_usb_dev_seacher.get_connected_USB_dev_numbers_from_target_strs(target_usb_storage_strs)
