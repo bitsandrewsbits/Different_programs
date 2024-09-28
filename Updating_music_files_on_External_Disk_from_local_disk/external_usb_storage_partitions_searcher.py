3a292fe9546bbf9cc92f391468582ecfc1899b49
Andrew Kovalenko <kov.andreas.30@gmail.com>
2024-09-28 22:09:16 +0300

    create method for External Partitions Searcher class - get output string from lsblk command. Also little change in init method.

--- Updating_music_files_on_External_Disk_from_local_disk/external_usb_storage_partitions_searcher.py
+++ Updating_music_files_on_External_Disk_from_local_disk/external_usb_storage_partitions_searcher.py
@@ -1,12 +1,13 @@
 # Class that searching on selected usb storage device all partitions
 # and show it
 import re
+import subprocess as sp
 
 class External_USB_Storage_Partitions_Searcher:
-	def __init__(self, all_connected_usb_storage_devices: list, selected_usb_storage_device: dict):
+	def __init__(self, all_connected_usb_storage_devices = [], selected_usb_storage_device = {}):
 		self.all_connected_usb_storage_devices = all_connected_usb_storage_devices
 		self.selected_usb_storage_device = selected_usb_storage_device
-		self.usb_partitions_searching_cmd = 'lsblk | grep "sd[b-z]"'
+		self.lsblk_cmd = ["lsblk"]
 		self.all_partitions_of_all_connected_usb_storage_devs = []
 		self.all_partitions_of_selected_usb_dev = []
 
@@ -16,6 +17,17 @@ class External_USB_Storage_Partitions_Searcher:
 	def find_all_partitions_of_selected_usb_device(self):
 		pass
 
-	def parse_lsblk_cmd(self):
+	def parse_output_lsblk_output_strings(self):
 		pass
+
+	def get_output_strings_from_lsblk_cmd(self):
+		bash_command_output = sp.run(self.lsblk_cmd, capture_output = True, text = True)
+		bash_command_result_strs = bash_command_output.stdout.strip()
+
+		return bash_command_result_strs
 	
+
+if __name__ == '__main__':
+	external_usb_storage_partitions_seacher = External_USB_Storage_Partitions_Searcher()
+
+	external_usb_storage_partitions_seacher.execute_target_lsblk_cmd()
